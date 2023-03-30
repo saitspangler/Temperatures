403f39d8f1b1c6a00853994377f765ff6b2c3b89
saitspangler <peter.spangler@edu.sait.ca>
2023-03-30 13:01:49 -0600

    Add project files.

---
+++ Temperatures/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Temperatures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstTemperatures = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtNewTemp = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNrDays = new System.Windows.Forms.TextBox();
+            this.txtAverage = new System.Windows.Forms.TextBox();
+            this.txtHotDays = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstTemperatures
+            // 
+            this.lstTemperatures.FormattingEnabled = true;
+            this.lstTemperatures.Location = new System.Drawing.Point(391, 74);
+            this.lstTemperatures.Name = "lstTemperatures";
+            this.lstTemperatures.Size = new System.Drawing.Size(186, 316);
+            this.lstTemperatures.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.txtNewTemp);
+            this.groupBox1.Location = new System.Drawing.Point(74, 47);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(204, 76);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "New Tempatures";
+            // 
+            // txtNewTemp
+            // 
+            this.txtNewTemp.Location = new System.Drawing.Point(7, 39);
+            this.txtNewTemp.Name = "txtNewTemp";
+            this.txtNewTemp.Size = new System.Drawing.Size(100, 20);
+            this.txtNewTemp.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(119, 36);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtHotDays);
+            this.groupBox2.Controls.Add(this.txtAverage);
+            this.groupBox2.Controls.Add(this.txtNrDays);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(74, 221);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(254, 169);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Statistics";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nr of Days";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Average Temperature";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(388, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Temperatures";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Days Above Avg";
+            // 
+            // txtNrDays
+            // 
+            this.txtNrDays.Location = new System.Drawing.Point(136, 45);
+            this.txtNrDays.Name = "txtNrDays";
+            this.txtNrDays.ReadOnly = true;
+            this.txtNrDays.Size = new System.Drawing.Size(100, 20);
+            this.txtNrDays.TabIndex = 3;
+            // 
+            // txtAverage
+            // 
+            this.txtAverage.Location = new System.Drawing.Point(136, 78);
+            this.txtAverage.Name = "txtAverage";
+            this.txtAverage.ReadOnly = true;
+            this.txtAverage.Size = new System.Drawing.Size(100, 20);
+            this.txtAverage.TabIndex = 4;
+            // 
+            // txtHotDays
+            // 
+            this.txtHotDays.Location = new System.Drawing.Point(136, 120);
+            this.txtHotDays.Name = "txtHotDays";
+            this.txtHotDays.ReadOnly = true;
+            this.txtHotDays.Size = new System.Drawing.Size(100, 20);
+            this.txtHotDays.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 449);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lstTemperatures);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstTemperatures;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtNewTemp;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtHotDays;
+        private System.Windows.Forms.TextBox txtAverage;
+        private System.Windows.Forms.TextBox txtNrDays;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Temperatures/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Temperatures
+{
+    /*
+     * Collects Temperatures daily for up to a month
+     * Keeps track of number of days, average temperature, and
+     * how many days are above average
+     * 
+     * Author: Peter
+     * When: March
+     */
+    public partial class Form1 : Form
+    {
+        // class level declarations
+        const int MAX_DAYS = 31;
+        int[] temperatures = new int[MAX_DAYS];
+        int nrDays = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Add one more temperature
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //read next temperature 
+        }
+    }
+}
